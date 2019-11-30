de7b269389084ac82f63655df6aac70832af304d
shuma1020 <shumaseki40@gmail.com>
2019-12-01 08:48:43 +0900

    fix seven

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
     has_many :rewards
     validates :status, presence: true
+    validates :title, presence: true
+    validates :price, presence: true
+    validates :description, presence: true
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,3 +1,6 @@
 class Reward < ApplicationRecord
     belongs_to :product
+    validates :title, presence: true
+    validates :price, presence: true
+    validates :description, presence: true
 end
