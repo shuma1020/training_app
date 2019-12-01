884fa69ad1252389446ba70a7c6e426f9b6ddf50
shuma1020 <shumaseki40@gmail.com>
2019-12-01 11:39:41 +0900

    adding gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users
   resources :rewards
   resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "products#index"
+
+  devise_scope :user do
+  get 'login', to: 'devise/sessions#new'
+  post 'login', to: 'devise/sessions#create'
+  delete 'signout', to: 'devise/sessions#destroy'  
+  end
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "products#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_002102) do
+ActiveRecord::Schema.define(version: 2019_12_01_004150) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 2019_11_30_002102) do
     t.integer "product_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
