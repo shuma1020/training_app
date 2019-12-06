dcedef6d2a3183ed1aa296000b16b39a87b414f2
shuma1020 <shumaseki40@gmail.com>
2019-12-06 20:48:16 +0900

    turn index to login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   before_action :user_login
+  
   # before_action :configure_sign_in_params, only: [:create]
   
   # GET /resource/sign_in
