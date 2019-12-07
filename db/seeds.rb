93d59fde4ce335b03a5c7613f20f2fb03956fb7a
shuma1020 <shumaseki40@gmail.com>
2019-12-07 14:03:40 +0900

    seed to symbol

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@ user1 = User.create!(id: 1, email: "user1@example.com", password: "password", na
 user2 = User.create!(id: 2, email: "user2@example.com", password: "password", name: "gggg")
 user3 = User.create!(id: 3, email: "user３@example.com", password: "password", name: "user3")
 
-pro1 = user1.products.create!(title: "aaa", price:100, description: "33333", status: "release", message: "gggg")
-pro2 = user2.products.create!(title: "kaka", price:100, description: "eeee", status: "release",  message: "eeee")
-pro3 = user2.products.create!(title: "ronald", price:100, description: "eeee", status: "release",  message: "eeee")
+pro1 = user1.products.create!(title: "aaa", price:100, description: "33333", status: :release, message: "gggg")
+pro2 = user2.products.create!(title: "kaka", price:100, description: "eeee", status: :release,  message: "eeee")
+pro3 = user2.products.create!(title: "ronald", price:100, description: "eeee", status: :release,  message: "eeee")
 
 reward1 = pro2.rewards.create!(title: "aaa", price: 100, description: "3333")
 reward2 = pro3.rewards.create!(title: "bbb", price: 100, description: "3333")
@@ -19,8 +19,8 @@ reward3 = pro3.rewards.create!(title: "ccc", price: 100, description: "3333")
 reward4 = pro3.rewards.create!(title: "ccc", price: 100, description: "3333")
 
 
-pro4 = user3.products.create!(title: "messi", price:100, description: "eeee", status: "draft",  message: "eeee")
-pro5 = user3.products.create!(title: "honda", price:100, description: "eeee", status: "draft",  message: "eeee")
+pro4 = user3.products.create!(title: "messi", price:100, description: "eeee", status: :draft,  message: "eeee")
+pro5 = user3.products.create!(title: "honda", price:100, description: "eeee", status: :draft,  message: "eeee")
 
 reward5 = pro4.rewards.create!(title: "nono", price: 100, description: "3333")
 reward6 = pro5.rewards.create!(title: "nana", price: 100, description: "3333")
