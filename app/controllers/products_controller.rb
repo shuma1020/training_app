aab71c77f3495fab55b68c8e2f297f3832ef62cd
shuma1020 <shumaseki40@gmail.com>
2019-12-09 22:53:15 +0900

    tentive locale i18n

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,7 +27,7 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
-    @product.user_id=current_user.id
+    #current_user.id = @product.user.id
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
