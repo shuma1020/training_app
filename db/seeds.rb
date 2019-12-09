d463da35412ee1a9efbec29dc609cb534bcfc2ff
shuma1020 <shumaseki40@gmail.com>
2019-12-09 15:39:41 +0900

    remove setting from controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user1 = User.create!(id: 1, email: "user1@example.com", password: "password", name: "shuma", role: :normal)
-user2 = User.create!(id: 2, email: "user2@example.com", password: "password", name: "gggg", role: :normal)
-user3 = User.create!(id: 3, email: "user３@example.com", password: "password", name: "user3", role: :staff)
+user2 = User.create!(id: 2, email: "user2@example.com", password: "password", name: "gggg", role: :staff)
+user3 = User.create!(id: 3, email: "user３@example.com", password: "password", name: "user3", role: :normal)
 
 pro1 = user1.products.create!(title: "aaa", price:100, description: "33333", status: "release", message: "gggg")
 pro2 = user2.products.create!(title: "kaka", price:100, description: "eeee", status: "release",  message: "eeee")
