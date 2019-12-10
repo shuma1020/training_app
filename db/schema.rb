b5e4bcf9b17001f333d3121376c3167f94b16c40
shuma1020 <shumaseki40@gmail.com>
2019-12-10 23:43:00 +0900

    delete patron_id etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_121756) do
+ActiveRecord::Schema.define(version: 2019_12_10_144121) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2019_12_10_121756) do
     t.integer "status", default: 0, null: false
     t.integer "user_id", default: 0
     t.string "image"
-    t.integer "patron_id"
   end
 
   create_table "rewards", force: :cascade do |t|
