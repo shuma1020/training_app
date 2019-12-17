651372780f445dffdefea37eef31c93b1f961ab3
shuma1020 <shumaseki40@gmail.com>
2019-12-17 14:49:25 +0900

    fix destroy redirect path

--- app/controllers/mypage/products_controller.rb
+++ app/controllers/mypage/products_controller.rb
@@ -42,7 +42,7 @@ class Mypage::ProductsController < ApplicationController
     def destroy
         @product.destroy
         respond_to do |format|
-          format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+          format.html { redirect_to mypage_products_path, notice: 'Product was successfully destroyed.' }
           format.json { head :no_content }
         end
     end
