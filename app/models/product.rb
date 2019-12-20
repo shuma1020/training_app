0240a5bae48cee6c14cd424332324f7442570654
shuma1020 <shumaseki40@gmail.com>
2019-12-20 17:16:32 +0900

    516

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,6 @@ class Product < ApplicationRecord
     mount_uploader :image, ImageUploader
     has_many :agents
     has_many :patrons, through: :agents, source: :user
+    has_many :likes, dependent: :destroy
+    has_many :likeres, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,12 @@ class User < ApplicationRecord
   enum role: { normal: 0, staff: 1 }
   has_many :agents
   has_many :items, through: :agents, source: :product
+  has_many :likes, dependent: :destroy
+  has_many :liked_products, through: :likes, source: :product
 
+  def liked_for?(product)
+    product && product.user != self && !likes.exists?(product_id: product.id)
+  end
   def patron_for?
     current_user.id == Product.find(params[:id])
   end
