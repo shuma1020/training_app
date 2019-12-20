370838a2a92d67fdc3943bc548ab9e588fcddbf0
shuma1020 <shumaseki40@gmail.com>
2019-12-20 17:05:00 +0900

    504

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,16 @@ Rails.application.routes.draw do
       get'draft'
       get'release'
     end
+    resources :agents, only: [:destroy, :create]
+    resources :rewards, only: [:index]
     patch "like", "unlike", on: :member
     get "liked", on: :collection
   end
+  resources :rewards
   devise_scope :user do
   get 'login', to: 'devise/sessions#new'
   post 'login', to: 'devise/sessions#create'
-  delete 'signout', to: 'devise/sessions#destroy'  
+  delete 'signout', to: 'devise/sessions#destroy'
   end
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "products#index"
@@ -21,8 +24,5 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-  namespace :mypage do
-    resources :products
-  end
-  resources :rewards
+  get 'patron/:id', to: 'products#patron'
 end
