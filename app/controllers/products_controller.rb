018938bb215e6998eb704c9c1495687d51bec7da
shuma1020 <shumaseki40@gmail.com>
2019-12-20 09:59:21 +0900

    fix about indent

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -92,7 +92,6 @@ class ProductsController < ApplicationController
     end
 
 
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:title, :price, :description, :message, :status, :user_id, :image, :image_cache, :remove_image)
