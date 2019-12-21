ecfb69bd8cfa9f5602c60112c7e07ad48b4cb9ec
shuma1020 <shumaseki40@gmail.com>
2019-12-21 15:24:30 +0900

    remove public upload from git

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,12 @@
 class Like < ApplicationRecord
     belongs_to :user
     belongs_to :product
-    
+
     validate do
         unless user && user.liked_for?(product)
-           puts "いいねできません"
+            errors.add(:base, "いいねはできません！")
         end
     end
 
-    
+
 end
