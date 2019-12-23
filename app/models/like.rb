94fbf8223cc805a302ea03df7c414ed497ab09de
shuma1020 <shumaseki40@gmail.com>
2019-12-23 19:17:17 +0900

    change validate of like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,11 @@
 class Like < ApplicationRecord
     belongs_to :user
     belongs_to :product
+    validates :check_liked
 
-    validate do
+    def check_liked
         unless user && user.liked_for?(product)
             errors.add(:base, "いいねはできません！")
         end
     end
-
-
 end
