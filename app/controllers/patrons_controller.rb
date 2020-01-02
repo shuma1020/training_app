1a4efda19f2fe316e5a4de06db6dac1a8db74cee
shuma1020 <shumaseki40@gmail.com>
2020-01-02 21:47:48 +0900

    error message hi

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -8,15 +8,16 @@ class PatronsController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
+    @rewards = @product.rewards
     @patron = @product.patrons.new(patron_params)
     @patron.user_id = current_user.id
-    respond_to do |format|
+      respond_to do |format|
       if @patron.save
         format.html { redirect_to @product, notice: 'パトロンになりました' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
-        format.json { render json: @product.errors, notice: '金額を入れてください' }
+        format.json { render json: @patron.errors, status: :unprocessable_entity }
       end
     end
   end
