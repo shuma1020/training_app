633f9c61540ac68eb069342550b7f569fe117b08
shuma1020 <shumaseki40@gmail.com>
2020-01-10 14:45:17 +0900

    improve index

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -15,24 +15,23 @@ class PatronsController < ApplicationController
       if @patron.donation >= @rewards.maximum(:price)
         @reward = @rewards.find_by(price: @rewards.maximum(:price))
         @patron.reward_id= @reward.id
-          if @patron.save
-            format.html { redirect_to @product, notice: 'パトロンになりました' }
-            format.json { render :show, status: :created, location: @product }
-          else
-            format.html { render :new }
-            format.json { render json: @patron.errors, status: :unprocessable_entity }
-          end
-
+        if @patron.save
+          format.html { redirect_to @product, notice: 'パトロンになりました' }
+          format.json { render :show, status: :created, location: @product }
+        else
+          format.html { render :new }
+          format.json { render json: @patron.errors, status: :unprocessable_entity }
+        end
       elsif @rewards.median(:price) <= @patron.donation && @patron.donation < @rewards.maximum(:price)
         @reward = @rewards.find_by(price: @rewards.median(:price))
         @patron.reward_id= @reward.id
-          if @patron.save
-            format.html { redirect_to @product, notice: 'パトロンになりました' }
-            format.json { render :show, status: :created, location: @product }
-          else
-            format.html { render :new }
-            format.json { render json: @patron.errors, status: :unprocessable_entity }
-          end
+        if @patron.save
+          format.html { redirect_to @product, notice: 'パトロンになりました' }
+          format.json { render :show, status: :created, location: @product }
+        else
+          format.html { render :new }
+          format.json { render json: @patron.errors, status: :unprocessable_entity }
+        end
       else
         @reward = @rewards.find_by(price: @rewards.minimum(:price))
         @patron.reward_id= @reward.id
