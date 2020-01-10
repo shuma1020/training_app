3e1f9819a45945e76840041efa0265aadb9f4adf
shuma1020 <shumaseki40@gmail.com>
2020-01-10 12:02:59 +0900

    if no naka

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -8,24 +8,31 @@ class PatronsController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
-    @rewards = @product.rewards
     @patron = @product.patrons.new(patron_params)
     @patron.user_id = current_user.id
+    @rewards = @product.rewards
     respond_to do |format|
       if @patron.donation >= @rewards.maximum(:price)
-      if @reward.patron.save
-        format.html { redirect_to @product, notice: 'パトロンになりました' }
-        format.json { render :show, status: :created, location: @product }
+        @reward = @rewards.find_by(price: @rewards.maximum(:price))
+        @patron.reward_id= @reward.id
+        if @patron.save
+          format.html { redirect_to @product, notice: 'パトロンになりました' }
+          format.json { render :show, status: :created, location: @product }
+        else
+          format.html { render :new }
+          format.json { render json: @patron.errors, status: :unprocessable_entity }
+        end
+      elsif  @rewards.minimum(:price) <= @patron.donation <= @rewards.maximum(:price)
+        p "####"
       else
-        format.html { render :new }
-        format.json { render json: @patron.errors, status: :unprocessable_entity }
+
       end
     end
   end
 
   private
     def patron_params
-      params.require(:patron).permit(:donation, :user_id, :product_id)
+      params.require(:patron).permit(:donation, :user_id, :product_id, :reward_id)
     end
 
 end
