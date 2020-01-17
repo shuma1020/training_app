c55661fc1ed9102121a0fe25bab26cf78ff36aad
shuma1020 <shumaseki40@gmail.com>
2020-01-17 14:56:29 +0900

    okok

--- app/mailers/patron_mailer.rb
+++ app/mailers/patron_mailer.rb
@@ -3,19 +3,19 @@ class PatronMailer < ApplicationMailer
   def notification_for_patron(patron)
     @patron = patron
     @user = @patron.user
-    mail(subject: "パトロンになったことを確認いたしました。",to: @user.email)
+    if mail(subject: "パトロンになったことを確認いたしました。",to: @user.email)
+      @notification = @patron.notifications.new(user_id: @user.id)
+      @notification.save
+    end
   end
 
   def notification_for_owner(patron)
     @patron = patron
     @user = @patron.user
     @product = @patron.product
-    @notification = @patron.notifications.new(user_id: @user.id)
-    @notification.save
     if mail(subject: "#{@user.email}さんが#{@product.title}のパトロンになったことを確認いたしました。",to: @product.user.email)
-
-    else
-      puts "確認用のメールは送信できませんでした。"
+      @notification = @patron.notifications.new(user_id: @product.user_id)
+      @notification.save
     end
   end
 end
