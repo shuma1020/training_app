a5dce16413814203262b71cd3132d98f9eb1e5dd
shuma1020 <shumaseki40@gmail.com>
2020-01-22 14:37:01 +0900

    made to product owner

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,8 +1,9 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :patron
-
-  def check_notification_user?
+  
+private
+  def to_product_owner?
     self.user_id == self.patron.product.user_id
   end
 end
