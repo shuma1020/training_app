7d701aba857bd29f9ddf200941891875aad4c739
shuma1020 <shumaseki40@gmail.com>
2020-01-23 15:28:21 +0900

    unread turn to read

--- app/controllers/mypage/notifications_controller.rb
+++ app/controllers/mypage/notifications_controller.rb
@@ -2,5 +2,7 @@ class Mypage::NotificationsController < ApplicationController
 
   def index
     @notifications = current_user.notifications
+    @unread = @notifications.where(status:"unread")
+    @unread.update(status:"read")
   end
 end
