f96762816d21604d260028970d7fd99142ddafc1
shuma1020 <shumaseki40@gmail.com>
2020-01-23 15:10:17 +0900

    add status to notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :patron
-
+  enum status: {unread: 0, read:1}
   def to_product_owner?
     self.user_id == self.patron.product.user_id
   end
