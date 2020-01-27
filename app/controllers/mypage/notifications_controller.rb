68d8b53e9c609de7cc1d06353a7f73ba368c1ee1
shuma1020 <shumaseki40@gmail.com>
2020-01-27 14:40:44 +0900

    unread

--- app/controllers/mypage/notifications_controller.rb
+++ app/controllers/mypage/notifications_controller.rb
@@ -2,7 +2,7 @@ class Mypage::NotificationsController < ApplicationController
 
   def index
     @notifications = current_user.notifications
-    unread = @notifications.where(status:"unread")
+    unread = @notifications.unread
     unread.update(status:"read")
     @count_unread = unread.count
   end
