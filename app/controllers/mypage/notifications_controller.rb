ab58bd4bfa8c10c4e00b0a2c8984edcf7e5d219f
shuma1020 <shumaseki40@gmail.com>
2020-01-30 17:01:32 +0900

    mypagenotification enum read

--- app/controllers/mypage/notifications_controller.rb
+++ app/controllers/mypage/notifications_controller.rb
@@ -3,7 +3,9 @@ class Mypage::NotificationsController < ApplicationController
   def index
     @notifications = current_user.notifications
     unread = @notifications.unread
-    unread.update(status:"read")
+    @notifications.each do |notification|
+      notification.read!
+    end
     @count_unread = unread.count
   end
 end
