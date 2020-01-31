c92aafdca05df21f0835c43536e249195fd8363f
shuma1020 <shumaseki40@gmail.com>
2020-01-31 21:54:57 +0900

    kaminari bootstrap

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.page(params[:page]).per(5)
   end
 
   # GET /products/1
