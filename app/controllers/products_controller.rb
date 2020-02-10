df68a5915e0cbe0e240cc36fd23249d0896152e8
shuma1020 <shumaseki40@gmail.com>
2020-02-10 10:30:52 +0900

    search form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.where("title LIKE?", "#{params[:title]}")
   end
 
   # GET /products/1
