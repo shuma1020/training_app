dc214040165f5d78ad1ebb904c25bddda81fd7f0
shuma1020 <shumaseki40@gmail.com>
2020-02-10 15:28:18 +0900

    add factroy_bot

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,6 @@ module SampleApp
         config.load_defaults 5.1
         config.generators do |g|
           g.test_framework :rspec,
-          fixtures: false,
           view_specs: false,
           helper_specs: false,
           routing_specs: false
