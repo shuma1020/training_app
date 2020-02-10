84679fef462d920053d7dc9ac4f2e71d95e090df
shuma1020 <shumaseki40@gmail.com>
2020-02-10 19:30:31 +0900

    user in factory

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a factory" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
 end
