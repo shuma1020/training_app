9136234fcd67bd3e559e85679db2eec1dfd6e3fb
shuma1020 <shumaseki40@gmail.com>
2020-02-10 09:49:12 +0900

    gem bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,5 +13,8 @@ require("channels")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
