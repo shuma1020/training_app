18432dd59b59793f96d285766c5b438dbcaadbb3
shuma1020 <shumaseki40@gmail.com>
2020-02-11 14:35:15 +0900

    not succeed

---
+++ spec/factories/products.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :product do
+    association :user
+    title "好きを仕事に"
+    price 200
+    image Rack::Test::UploadedFile.new(File.join('app/assets/images/image.png'))
+    description "aaaaaaaaaaa"
+
+  end
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Product, type: :model do
   it "has a factory" do
     expect(FactoryBot.build(:user)).to be_valid
   end
+
+  it "has rewards when you manage it as realese" do
+    product = FactoryBot.create(:product)
+    product.valid?
+    expect(product.errors[:title]).to include("バリデーションに失敗しました: タイトルを入力してください")
+  end
 end
