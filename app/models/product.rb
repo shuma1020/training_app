4bca1197179b72a4846efcf793ddb16029daea5f
shuma1020 <shumaseki40@gmail.com>
2020-02-13 09:49:34 +0900

    layout4

--- app/models/product.rb
+++ app/models/product.rb
@@ -24,7 +24,13 @@ class Product < ApplicationRecord
         self.user_id == user.id
     end
 
-    def self.count_donation(patrons)
+    def count_donation
+        @patrons = self.patrons.all
         patrons.sum{|patron|patron[:donation]}
     end
+
+    def self.percent_of_donation(dontaion, product)
+        (dontaion.to_f / product.price * 100).round(0)
+    end
+
 end
