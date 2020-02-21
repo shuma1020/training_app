9c32b4a31f363d800ac05b31532562dff72b359b
shuma1020 <shumaseki40@gmail.com>
2020-02-21 17:09:24 +0900

    test relating rewards

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -3,12 +3,20 @@ require 'rails_helper'
 RSpec.describe Reward, type: :model do
   it "has a limit which allows only three " do
     product = FactoryBot.create(:product)
-    reward1 = product.rewards.build(title:"ccc",description:"aaaa", price:200)
-    reward1.valid?
+    reward1 = product.rewards.create(title:"ccc",description:"aaaa", price:200)
     reward2 = product.rewards.create(title:"ccc",description:"aaaa", price:200)
     reward3 = product.rewards.create(title:"ccc",description:"aaaa", price:200)
-    reward4 = product.rewards.build(title:"ccc",description:"aaaa", price:200)
-    reward4.valid?
+    reward4 = product.rewards.create(title:"ccc",description:"aaaa", price:200)
     expect(reward4.errors[:base]).to include("設定できるRewardは３つまでです")
+  end
+
+  it "has a limit which allows only three " do
+    product = FactoryBot.create(:product)
+    reward1 = product.rewards.create(title:"ccc",description:"aaaa", price:200)
+    reward2 = product.rewards.create(title:"ccc",description:"aaaa", price:200)
+    reward3 = product.rewards.create(title:"ccc",description:"aaaa", price:200)
+    reward3.price = 3000
+    reward3.save!
+    expect(reward3.errors[:base]).to_not include("設定できるRewardは３つまでです")
   end
 end
