6af7b452530d9d3549ffab68aed305ca5d7fff75
shuma1020 <shumaseki40@gmail.com>
2020-02-26 15:30:18 +0900

    fix a bit

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe Product, type: :model do
   end
 
   it "arrange patrons desc when it has no patron" do
-    product = FactoryBot.create(:product)
     products =  Product.joins(:patrons).group(:product_id).select('count(donation) as patron_count').order('patron_count desc')
     expect(products).to be_empty
   end
